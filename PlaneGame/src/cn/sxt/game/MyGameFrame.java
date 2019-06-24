420400aadb96150a25adf23f87f03b302c1df789
FireflySky <2366297257@qq.com>
2019-06-24 13:32:45 +0800

    add PlaneGame

---
+++ PlaneGame/src/cn/sxt/game/MyGameFrame.java
@@ -0,0 +1,146 @@
+package cn.sxt.game;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Date;
+
+/**
+ * �ɻ���Ϸ��������
+ * @author ���
+ *
+ */
+public class MyGameFrame  extends  Frame {
+	
+	Image   planeImg  = GameUtil.getImage("images/plane.png");
+	Image   bg  = GameUtil.getImage("images/bg.jpg");
+	
+	Plane   plane = new Plane(planeImg,250,250);
+	Shell[]   shells = new Shell[50];/**子弹*/
+	
+	Explode   bao ;
+	Date  startTime = new Date();
+	Date  endTime;
+	int period;   //移动距离
+	
+	@Override
+	public void paint(Graphics g) {		//画笔，自动调用
+		Color   c =  g.getColor();
+		g.drawImage(bg, 0, 0, null);
+		
+		plane.drawSelf(g);  //绘制飞机
+		
+		//绘制子弹
+		for(int i=0;i<shells.length;i++){
+			shells[i].draw(g);
+			
+			//爆炸
+			boolean  peng = shells[i].getRect().intersects(plane.getRect());
+			if(peng){
+				plane.live = false;
+				if(bao ==null){
+					bao  = new Explode(plane.x, plane.y);		
+					endTime = new Date();
+					period = (int)((endTime.getTime()-startTime.getTime())/1000);
+				}
+				bao.draw(g);
+			}
+			
+			//字体设置
+			if(!plane.live){
+				g.setColor(Color.red);
+				Font   f  =  new Font("微软雅黑", Font.BOLD, 50);
+				g.setFont(f);
+				g.drawString("用时"+period+"秒", (int)Constant.GAME_WIDTH/2-100, (int)Constant.GAME_HEIGHT/2);
+			}
+			
+		}
+		
+		g.setColor(c);
+	}
+	
+	
+	//线程
+	class  PaintThread  extends  Thread  {
+		@Override
+		public void run() {
+			while(true){
+				repaint();		//重新绘制
+				
+				try {
+					Thread.sleep(40);   	//1s=1000ms
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}		
+			}
+		}
+		
+	}
+	
+	//键盘的按键事件
+	class   KeyMonitor extends  KeyAdapter  {
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			plane.addDirection(e);
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			plane.minusDirection(e);
+		}
+		
+		
+	}
+	
+	
+	/**
+	 * ��ʼ������
+	 */
+	public  void  launchFrame(){
+		this.setTitle("飞机小游戏");
+		this.setVisible(true);
+		this.setSize(Constant.GAME_WIDTH	, Constant.GAME_HEIGHT);
+		this.setLocation(300, 300);
+		
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		new PaintThread().start();	//�����ػ����ڵ��߳�
+		addKeyListener(new KeyMonitor());   //���������Ӽ��̵ļ���
+		
+		
+		//��ʼ��50���ڵ�
+		for(int i=0;i<shells.length;i++){
+			shells[i] = new Shell();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		MyGameFrame  f = new MyGameFrame();
+		f.launchFrame();
+	}
+	
+	private Image offScreenImage = null;
+	 
+	public void update(Graphics g) {
+	    if(offScreenImage == null)
+	        offScreenImage = this.createImage(Constant.GAME_WIDTH,Constant.GAME_HEIGHT);//������Ϸ���ڵĿ�Ⱥ͸߶�
+	     
+	    Graphics gOff = offScreenImage.getGraphics();
+	    paint(gOff);
+	    g.drawImage(offScreenImage, 0, 0, null);
+	}
+	
+}
---
+++ PlaneGame/src/cn/sxt/game/Plane.java
@@ -0,0 +1,83 @@
+package cn.sxt.game;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+
+public class Plane  extends GameObject {
+	boolean  left,up,right,down;
+	
+	boolean  live = true;  
+	//飞机移动
+	public  void  drawSelf(Graphics  g){
+		if(live){//判断飞机是否还活着
+				g.drawImage(img, (int)x,(int) y, null);
+				
+				if(left){
+					x -=speed;
+				}
+				if(right){
+					x += speed;
+				}
+				if(up){
+					y -=speed;    //y = y-speed;
+				}
+				if(down){
+					y += speed;
+			}
+		}else{
+			
+		}
+		
+		
+		
+	}
+	//初始化
+	public  Plane(Image  img, double x, double y){
+		this.img = img;
+		this.x = x;
+		this.y = y;
+		this.speed = 3;
+		this.width = img.getWidth(null) ;
+		this.height = img.getHeight(null);
+		
+	}
+	
+	//按键按下事件
+	public  void   addDirection(KeyEvent  e){
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_A:
+			left = true;
+			break;
+		case KeyEvent.VK_W:
+			up = true;
+			break;
+		case KeyEvent.VK_D:
+			right = true;
+			break;
+		case KeyEvent.VK_S:
+			down = true;
+			break;
+		}
+	}
+	
+	//按键抬起事件
+		public  void   minusDirection(KeyEvent  e){
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_A:
+				left = false;
+				break;
+			case KeyEvent.VK_W:
+				up = false;
+				break;
+			case KeyEvent.VK_D:
+				right = false;
+				break;
+			case KeyEvent.VK_S:
+				down = false;
+				break;
+			}
+		}
+	
+	
+}
